903f9f007aeaabe11edc0a074a5ea04ccd037784
tushita <mercuriodevelop@gmail.com>
2018-03-24 06:41:50 -0600

    test

--- server/app.js
+++ server/app.js
@@ -35,7 +35,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/user',userRoutes);
 app.use('/user/profiles', profileRoutes);

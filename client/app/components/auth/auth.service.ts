14081d8fe0270d472d4dd92c8105db7d425d31b9
tushita <mercuriodevelop@gmail.com>
2018-03-24 09:16:43 -0600

    rock on

--- client/app/components/auth/auth.service.ts
+++ client/app/components/auth/auth.service.ts
@@ -5,17 +5,25 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Router } from '@angular/router';
 import 'rxjs/add/observable/forkJoin';
 import 'rxjs/add/operator/map';
+import { environmentProd } from '../../../environments/environment.prod';
 
+import { isDevMode } from '@angular/core';
 
 @Injectable()
-
 export class AuthService {
+
+serverUrl;
+
   constructor(private http: HttpClient, private router: Router) {
-    
+    if (isDevMode()) {
+      this.serverUrl = 'http://localhost:3000/';
+    } else {
+      this.serverUrl = environmentProd.server;
+    }
   }
 
   login(email: string, password: string) {
-    return this.http.post<any>('http://localhost:3000/user/login', { email: email, password: password })
+    return this.http.post<any>(this.serverUrl+'user/login', { email: email, password: password })
       .map(user => {
         // login successful if there's a jwt token in the response
         if (user && user.token) {

687b2fad6608135af2c836fee24c5b9f1e5778ca
tushita <mercuriodevelop@gmail.com>
2018-03-24 07:06:22 -0600

    test

--- server/app.js
+++ server/app.js
@@ -32,12 +32,16 @@ const forceSSL = function () {
 // middleware
 app.use(forceSSL());
 app.use(cors())
+
+
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function (req, res) {
-    res.sendFile(__dirname + '../dist/index.html');
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 app.use('/user',userRoutes);

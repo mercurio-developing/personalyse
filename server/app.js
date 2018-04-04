69e912f73e84e9d9f67995bafcae99d043b5607b
tushita <mercuriodevelop@gmail.com>
2018-04-04 09:44:13 -0600

    rock

--- server/app.js
+++ server/app.js
@@ -33,7 +33,7 @@ app.use('/api/youtube',youtubeRoutes);
 app.use('/api/spotify', spotifyRoutes);
 
 require('./db');
-
+require('./cron')
 app.listen(port, function () {
     console.log("Express Started on Port 3000");
 });

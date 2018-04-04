4e9ff3cc50b312a1ccd67301c7973009ca50a14b
tushita <mercuriodevelop@gmail.com>
2018-04-04 04:06:32 -0600

    rock

--- server/cron.js
+++ server/cron.js
@@ -39,7 +39,7 @@ const todo = () =>{
 }
 
 let daily = new cron.CronJob({
-    cronTime: '* * * * *',  
+    cronTime: '30 * * * *',  
         onTick: function () {
         console.log('running ')
         todo();

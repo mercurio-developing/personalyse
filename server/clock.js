3304bcb68624c6d2e1baa61ae08fc5eebece0520
tushita <mercuriodevelop@gmail.com>
2018-04-04 04:13:59 -0600

    rock

---
+++ server/clock.js
@@ -0,0 +1,11 @@
+var CronJob = require('cron').CronJob;
+var worker = require('./worker.js');
+
+var job = new CronJob({
+    cronTime: '30 * * * *',  
+    onTick: worker.todo(),
+    start: true,
+    timeZone: "America/Los_Angeles"
+});
+
+job.todo();

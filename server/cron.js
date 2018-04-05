9cddc5fa4e54f0f094c5299053f0c8f6e6355bdb
tushita <mercuriodevelop@gmail.com>
2018-04-05 11:40:26 -0600

    rock

--- server/cron.js
+++ server/cron.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const mongoose = require('mongoose')
-const CronJob  = require('cron').CronJob;
+const CronJob = require('cron').CronJob;
 
 const todoFace = require('./todo/todoFace.js');
 const todoTwit = require('./todo/todoTwit.js');
 const todoInst = require('./todo/todoInst.js');
-const todoYou  = require('./todo/todoYou.js');
+const todoYou = require('./todo/todoYou.js');
 const todoSpot = require('./todo/todoSpot.js');
 const todoSpotM = require('./todo/todoSpotM.js');
 
@@ -17,7 +17,7 @@ const Profile = require("./models/profile");
 
 const cron = require('cron');
 
-const todo = () =>{
+const todo = () => {
     let profilesArray;
     Profile.find({})
         .exec(function (err, profiles) {
@@ -26,21 +26,21 @@ const todo = () =>{
             } else {
                 profilesArray = profiles;
             }
-    profilesArray.forEach(function (profile) {
-        todoFace(profile.urls[0]);
-        todoInst(profile.urls[1], profile.artistId);
-        todoTwit(profile.urls[2],profile.artistId);
-        todoYou(profile.urls[3],profile.artistId)
-        todoVideo(profile.urls[3], profile.artistId)
-        todoSpot(profile.urls[4], profile.artistId)
-        // todoSpotM(profile.urls[4], profile.artistId);
-        }); 
-    });   
+            profilesArray.forEach(function (profile) {
+                todoFace(profile.urls[0]);
+                todoInst(profile.urls[1], profile.artistId);
+                todoTwit(profile.urls[2], profile.artistId);
+                todoYou(profile.urls[3], profile.artistId)
+                todoVideo(profile.urls[3], profile.artistId)
+                todoSpot(profile.urls[4], profile.artistId)
+                // todoSpotM(profile.urls[4], profile.artistId);
+            });
+        });
 }
 
 let daily = new cron.CronJob({
-    cronTime: '00 00 00 * * *',  
-        onTick: function () {
+    cronTime: '00 50 01 * * *',
+    onTick: function () {
         console.log('running ')
         todo();
     },
